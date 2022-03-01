e9faee181e307c3088a47c7eefabef4239bf56ed
Norton King <nortonking28@gmail.com>
2022-03-01 02:32:28 +0000

    added bars

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ function Cell(props) {
 	return(
 		<div className={"cell " + props.value} onClick={props.onClick}>
 			<div className="dot"></div>
-			
-			<div className="bar-container">
+			<div className="bar-container"  onClick={props.onBarClick}>
 			  <div className="bar-relative">
-				<div className="bar"></div>
+				<div className={"bar " + props.barValue} ></div>
 			  </div>
 			</div>
 		</div>
@@ -32,7 +31,8 @@ class Grid extends React.Component {
 			let foo = []
 			for (let i = 0; i < rows * columns; i++) {
 				foo.push({
-					color: "empty"
+					color: "empty",
+					bar: false
 				})
 			}
 			grid = foo
@@ -48,6 +48,7 @@ class Grid extends React.Component {
 	nukeGrid() {
 		let emptyGrid = this.state.grid
 		emptyGrid.forEach(e => e.color = "empty")
+		emptyGrid.forEach(e => e.bar = false)
 		
 		this.setState({
 			grid: emptyGrid
@@ -60,7 +61,7 @@ class Grid extends React.Component {
 	handleClick(i) {
 		let grid = this.state.grid.slice()
 		let cellColors = this.state.cellColors
-		
+
 		// Cycle through colors
 		let colorIndex = cellColors.indexOf(grid[i].color)
 		let newColorIndex = colorIndex + 1
@@ -73,19 +74,36 @@ class Grid extends React.Component {
 		this.setState({
 			grid: grid
 		})
+		localStorage.setItem("grid", JSON.stringify(grid))
+	}
+	
+	handleBarClick(e, i) {	
+		e.stopPropagation()
+		let grid = this.state.grid.slice()
+		grid[i].bar = !grid[i].bar
+		console.log(grid[i].bar)
 		
+		this.setState({
+			grid: grid
+		})
 		localStorage.setItem("grid", JSON.stringify(grid))
 	}
 
 	renderCell(i) {
 		return (
 			<Cell
+				// Dots
 				value={this.state.grid[i].color}
 				onClick={() => this.handleClick(i)}
+				// Bars
+				barValue={this.state.grid[i].bar}
+				onBarClick={(e) => this.handleBarClick(e, i)}
+				
 				key={i}
 			/>
 		)
 	}
+	
 	
 	render() {
 		return(
